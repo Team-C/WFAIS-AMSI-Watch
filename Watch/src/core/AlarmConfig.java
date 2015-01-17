6ed574dc3b0250a56a006098e814d429dec88229
Michal Szura <michal.szura@gmail.com>
2015-01-17 23:38:28 +0100

    Watch RC Update Missing are: - only icons indicating the Alarm and Signal States. - longPressA being sent as long as it's pressed.

--- Watch/src/core/AlarmConfig.java
+++ Watch/src/core/AlarmConfig.java
@@ -1,6 +1,13 @@
 package core;
 
 import enums.AlarmConfigState;
+import enums.AlarmState;
+import enums.SignalState;
+import enums.SoundState;
+import static enums.SoundState.ALARM_OFF_SOUND_OFF;
+import static enums.SoundState.ALARM_OFF_SOUND_ON;
+import static enums.SoundState.ALARM_ON_SOUND_OFF;
+import static enums.SoundState.ALARM_ON_SOUND_ON;
 import java.util.Calendar;
 
 /**
@@ -10,6 +17,7 @@ import java.util.Calendar;
 public class AlarmConfig implements Config<AlarmConfigState> {
 
     static AlarmConfigState configState = AlarmConfigState.DEFAULT;
+    static SoundState soundState = SoundState.ALARM_OFF_SOUND_OFF;
 
     @Override
     public void increaseTimeValue() {
@@ -27,6 +35,23 @@ public class AlarmConfig implements Config<AlarmConfigState> {
         }
     }
 
+    public void nextSoundState() {
+        soundState = soundState.nextState();
+        if (soundState == SoundState.ALARM_ON_SOUND_ON) {
+            core.Clock.alarmState = AlarmState.SET;
+            core.Clock.signalState = SignalState.SET;
+        } else if (soundState == SoundState.ALARM_ON_SOUND_OFF) {
+            core.Clock.alarmState = AlarmState.SET;
+            core.Clock.signalState = SignalState.OFF;
+        } else if (soundState == SoundState.ALARM_OFF_SOUND_ON) {
+            core.Clock.alarmState = AlarmState.OFF;
+            core.Clock.signalState = SignalState.SET;
+        } else {
+            core.Clock.alarmState = AlarmState.OFF;
+            core.Clock.signalState = SignalState.OFF;
+        }
+    }
+
     @Override
     public void cycleConfigState() {
         configState = configState.nextState();
@@ -35,6 +60,10 @@ public class AlarmConfig implements Config<AlarmConfigState> {
     @Override
     public AlarmConfigState getConfigState() {
         return configState;
+    }
+
+    public SoundState getSoundState() {
+        return soundState;
     }
 
     public void cycleAlarmStates() {
